67ec0cad5480fa44afe1d6f0b199fb8ba76f2406
Mart7anova <mart7anova@gmail.com>
2022-10-11 11:07:57 +0300

    feat: add Sign Up

--- src/features/auth/signUp/SignUp.tsx
+++ src/features/auth/signUp/SignUp.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import {useFormik} from 'formik';
-import {useAppDispatch} from '../../../common/hooks/useAppDispatch';
-import {authActions} from '../index';
 import FormControl from '@mui/material/FormControl';
+import {useAppDispatch} from '../../../common/hooks/useAppDispatch';
+import {useFormik} from 'formik';
 import {validateValuesForForm} from '../../../common/utils/validateValuesForForm';
-import {Button, Paper, TextField} from '@mui/material';
 import s from '../../../common/styles/Form.module.scss'
+import {Button, Paper, TextField} from '@mui/material';
 import {Password} from '../../../common/components/Password';
+import {authActions} from '../index';
 
+const {signUp} = authActions
 
-const {signIn} = authActions
-
-export const SignIn = () => {
+export const SignUp = () => {
     const dispatch = useAppDispatch()
 
     const formik = useFormik({
@@ -19,17 +18,19 @@ export const SignIn = () => {
             email: '',
             password: '',
             rememberMe: false,
+            confirmPassword: ''
         },
         validate: values => {
             return validateValuesForForm(values)
         },
         onSubmit: values => {
-            dispatch(signIn(values))
+            dispatch(signUp(values))
         },
     });
 
     const emailError = formik.errors.email && formik.touched.email
     const passError = formik.errors.password && formik.touched.password
+    const ConfirmPassError = formik.errors.confirmPassword && formik.touched.confirmPassword
 
     return (
         <form onSubmit={formik.handleSubmit} className={s.formContainer}>
@@ -38,19 +39,22 @@ export const SignIn = () => {
 
                     <h1 className={s.title}>Sing In</h1>
 
-                     <TextField variant={'standard'}
-                               sx={{m:2, mt: 5}}
-                               color={emailError ? 'error': 'primary'}
+                    <TextField variant={'standard'}
+                               sx={{m: 2, mt: 5}}
+                               color={emailError ? 'error' : 'primary'}
                                label={emailError ? formik.errors.email : 'Email'}
                                error={!!emailError}
-                                {...formik.getFieldProps('email')}
+                               {...formik.getFieldProps('email')}
                     />
 
-                    <Password passError={passError} formikErrorPass={formik.errors.password} {...formik.getFieldProps('password')}/>
+                    <Password passError={passError}
+                              formikErrorPass={formik.errors.password} {...formik.getFieldProps('password')}/>
+                    <Password passError={ConfirmPassError}
+                              formikErrorPass={formik.errors.confirmPassword} {...formik.getFieldProps('confirmPassword')}/>
 
                     <Button type={'submit'} variant={'contained'} sx={{m: 2}}>Sent</Button>
 
-                    <p className={s.textInfo}>Do not have account?</p>
+                    <p className={s.textInfo}>Already have an account?</p>
                 </FormControl>
             </Paper>
         </form>
