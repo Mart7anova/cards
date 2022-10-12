2978420e943b3cf8402c9ce0c482ef2d14050e6d
Mart7anova <mart7anova@gmail.com>
2022-10-12 15:25:32 +0300

    feat: add selectors for packs

---
+++ src/features/packs/selectors.ts
@@ -0,0 +1,14 @@
+import {AppRootStateType} from '../../app/store';
+
+export const getCardPacks = (state: AppRootStateType) => state.pack.packs.cardPacks
+export const getPagePacks = (state: AppRootStateType) => state.pack.packs.page
+export const getPageCountPacks = (state: AppRootStateType) => state.pack.packs.pageCount
+export const getCardPacksTotalCount = (state: AppRootStateType) => state.pack.packs.cardPacksTotalCount
+
+export const getMinCardsCountPacks = (state: AppRootStateType) => state.pack.packs.minCardsCount
+export const getMaxCardsCount = (state: AppRootStateType) => state.pack.packs.maxCardsCount
+
+
+export const getSearchParams= (state: AppRootStateType) => state.pack.searchParams
+export const getIsFirstLoading= (state: AppRootStateType) => state.pack.isFirstLoading
+export const getPacksStatus= (state: AppRootStateType) => state.pack.status
