e1b7c28f52f913b99531553ccb6e887ff88fabd8
Mart7anova <mart7anova@gmail.com>
2022-10-12 16:54:16 +0300

    fix: name selector

--- src/features/packs/selectors.ts
+++ src/features/packs/selectors.ts
@@ -5,7 +5,7 @@ export const getPagePacks = (state: AppRootStateType) => state.pack.packs.page
 export const getPageCountPacks = (state: AppRootStateType) => state.pack.packs.pageCount
 export const getCardPacksTotalCount = (state: AppRootStateType) => state.pack.packs.cardPacksTotalCount
 
-export const getMinCardsCountPacks = (state: AppRootStateType) => state.pack.packs.minCardsCount
+export const getMinCardsCount = (state: AppRootStateType) => state.pack.packs.minCardsCount
 export const getMaxCardsCount = (state: AppRootStateType) => state.pack.packs.maxCardsCount
 
 
