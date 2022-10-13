e188d4f655fe8408b4c1ccc347ceecc1e3ace655
Mart7anova <mart7anova@gmail.com>
2022-10-13 20:07:46 +0300

    feat: add search by card name

---
+++ src/features/cards/SearchByCardName/SearchByCardName.tsx
@@ -0,0 +1,33 @@
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import {TextField} from '@mui/material';
+import {useAppDispatch} from '../../../common/hooks/useAppDispatch';
+import {useDebounce} from '../../../common/hooks/useDebounce';
+import {cardActions} from '../index';
+
+const {setSearchParams} = cardActions
+
+export const SearchByCardName = () => {
+    const dispatch = useAppDispatch()
+
+    const [searchValue, setSearchValue] = useState('')
+    const debouncedValue = useDebounce<string>(searchValue, 500)
+
+    const onSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.currentTarget.value)
+    }
+
+    useEffect(() => {
+        dispatch(setSearchParams({cardQuestion: searchValue}))
+    }, [debouncedValue])
+    return (
+        <>
+            <h3>Search by question name</h3>
+            <TextField placeholder="Provide your text"
+                       size={'small'}
+                       value={searchValue}
+                       onChange={onSearchChange}
+                       style={{marginBottom: '20px', width: '100%'}}
+            />
+        </>
+    );
+};
