f5910185f2206875330f5c34d2bff437eab31a66
Mart7anova <mart7anova@gmail.com>
2022-10-14 14:03:55 +0300

    style: add mixins and Paper block

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,6 +1,5 @@
 import {Avatar, Button, FormControl, Paper} from '@mui/material';
 import React from 'react';
-import s from '../../assets/styles/Form.module.scss';
 import noUserPhoto from '../../assets/images/no-user-photo.png';
 import {useAppSelector} from '../../common/hooks/useAppSelector';
 import {getProfile} from './selectors';
@@ -8,6 +7,7 @@ import {authActions} from '../auth';
 import {useAppDispatch} from '../../common/hooks/useAppDispatch';
 import {EditableSpan} from '../../common/components/EditableSpan/EditableSpan';
 import {profileActions} from './index';
+import s from '../../assets/styles/PaperBlock.module.scss';
 
 const {signOut} = authActions
 const {updateProfile} = profileActions
@@ -25,22 +25,22 @@ export const Profile = () => {
     }
 
     return (
-        <div className={s.formContainer}>
+        <form className={s.mainContainer}>
             <Paper className={s.paper}>
                 <FormControl className={s.formItems}>
 
-                    <h1 className={s.title}>Personal Information</h1>
+                    <h1>Personal Information</h1>
 
                     <div className={s.flexContainer}>
                         <Avatar alt={name} src={avatar ? avatar : noUserPhoto} className={s.img}/>
                         <EditableSpan value={name} onChange={onNameChange}/>
                     </div>
 
-                    <p className={s.infoText} style={{textAlign: 'center'}}> {email} </p>
+                    <p className={s.infoText}> {email} </p>
 
                     <Button variant={'contained'} sx={{m: 2}} onClick={onClickHandler}>Log out</Button>
                 </FormControl>
             </Paper>
-        </div>
+        </form>
     );
 };
