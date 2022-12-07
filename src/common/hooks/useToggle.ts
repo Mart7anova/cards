16b1133f7202c4971a1b29fa7d87a12d34d8c5db
mart7anova <mart7anova7@gmail.com>
2022-12-07 13:44:07 +0300

    feat: add hook useToggle

---
+++ src/common/hooks/useToggle.ts
@@ -0,0 +1,7 @@
+import {useCallback, useState} from "react";
+
+export const useToggle = (initialState: boolean): [boolean, () => void] => {
+    const [isToggled, setIsToggled] = useState(initialState)
+    const toggle = useCallback(() => setIsToggled(!isToggled), [isToggled])
+    return [isToggled, toggle]
+}
