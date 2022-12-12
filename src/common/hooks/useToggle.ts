54078310519582c0db341f16a311981d79c2edb7
Mart7anova <mart7anova@gmail.com>
2022-12-12 11:33:59 +0300

    refactor: for prettier

--- src/common/hooks/useToggle.ts
+++ src/common/hooks/useToggle.ts
@@ -3,5 +3,6 @@ import { useCallback, useState } from 'react';
 export const useToggle = (initialState: boolean): [boolean, () => void] => {
   const [isToggled, setIsToggled] = useState(initialState);
   const toggle = useCallback(() => setIsToggled(!isToggled), [isToggled]);
+
   return [isToggled, toggle];
 };
