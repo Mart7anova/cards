a368ce830a21de8c8e328e1776813ebf2f998d71
Mart7anova <mart7anova@gmail.com>
2022-12-14 15:04:26 +0300

    style: change table cell width

--- src/features/packs/PacksTable/PackTableBody/PackTableBody.tsx
+++ src/features/packs/PacksTable/PackTableBody/PackTableBody.tsx
@@ -26,21 +26,21 @@ export const PackTableBody = (): ReactElement => {
           user_id: userId,
         }) => (
           <TableRow key={packId}>
-            <TableCell sx={{ overflowWrap: 'anywhere', width: '25%' }}>
+            <TableCell sx={{ overflowWrap: 'anywhere', width: '22%' }}>
               <CardPackTitle packId={packId} name={name} deckCover={deckCover} />
             </TableCell>
 
             <TableCell sx={{ width: '14%' }}>{cardsCount}</TableCell>
 
-            <TableCell sx={{ width: '20%' }}>
+            <TableCell sx={{ width: '18%' }}>
               {dayjs(updated).format(`DD.MM.YYYY`)}
             </TableCell>
 
-            <TableCell sx={{ overflowWrap: 'anywhere', width: '25%' }}>
+            <TableCell sx={{ overflowWrap: 'anywhere', width: '27%' }}>
               {userName}
             </TableCell>
 
-            <TableCell sx={{ width: '16%' }}>
+            <TableCell sx={{ width: '18%' }}>
               <PackIconsGroup
                 packUserId={userId}
                 packId={packId}
