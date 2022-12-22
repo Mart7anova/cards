cebe30e42f6ac6679d2995c508f0d4a2c55dfe93
Mart7anova <mart7anova@gmail.com>
2022-12-22 11:13:01 +0300

    refactor: add NavLink

--- src/common/components/NavBar/NavBar.tsx
+++ src/common/components/NavBar/NavBar.tsx
@@ -1,44 +1,32 @@
 import React, { ReactElement } from 'react';
 
-import { AppBar, Box, Button, Container } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import { AppBar, Box, Container } from '@mui/material';
 
 import { useAppSelector } from '../../hooks/useAppSelector';
-import { ProfileLink } from '../ProfileLink/ProfileLink';
-import style from '../ProfileLink/ProfileLink.module.scss';
 
 import c from './NavBar.module.scss';
 
+import { NavLink } from 'common/components/NavBar/NavLink/NavLink';
 import { Path } from 'common/enums/Path';
 import { selectIsLoggedIn } from 'features/auth/selectors';
 
 export const NavBar = (): ReactElement => {
-  const navigate = useNavigate();
-
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
-
-  const signInHandler = (): void => {
-    navigate(Path.SIGN_IN);
-  };
 
   return (
     <Box>
       <AppBar position="static" className={c.bar}>
         <Container fixed className={c.container}>
-          <Link to={Path.PACKS} className={style.userContainer}>
-            <h1 className={style.name}>Packs</h1>
-          </Link>
+          <NavLink link={Path.PACKS} title="Packs" />
 
-          <Link to={Path.USERS} className={style.userContainer}>
-            <h1 className={style.name}>Users</h1>
-          </Link>
+          <NavLink link={Path.CHAT} title="Chat" />
+
+          <NavLink link={Path.USERS} title="Users" />
 
           {isLoggedIn ? (
-            <ProfileLink />
+            <NavLink link={Path.PROFILE} title="My profile" />
           ) : (
-            <Button color="inherit" size="large" onClick={signInHandler}>
-              <h2>Sign in</h2>
-            </Button>
+            <NavLink link={Path.SIGN_IN} title="Sign in" />
           )}
         </Container>
       </AppBar>
---
+++ src/common/components/NavBar/NavLink/NavLink.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import style from 'common/components/NavBar/NavLink/NavLink.module.scss';
+
+type PropsType = {
+  title: string;
+  link: string;
+};
+
+export const NavLink = ({ title, link }: PropsType): ReactElement => {
+  return (
+    <Link to={link} className={style.userContainer}>
+      <h1 className={style.name}>{title}</h1>
+    </Link>
+  );
+};
